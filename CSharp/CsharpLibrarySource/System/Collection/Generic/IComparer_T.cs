d975fa30fd8dbce6813bc43c0ec2bc4c18b44564
woo sung jang <ridgody@hanmail.net>
2022-05-29 17:31:43 +0900

    add source

---
+++ CSharp/CsharpLibrarySource/System/Collection/Generic/IComparer_T.cs
@@ -0,0 +1,64 @@
+﻿1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+// ==++==
+// 
+//   Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+// ==--==
+/*============================================================
+**
+** Interface:  IComparer
+** 
+** <OWNER>kimhamil</OWNER>
+**
+**
+** Purpose: Interface for comparing two generic Objects.
+**
+** 
+===========================================================*/
+namespace System.Collections.Generic
+{
+
+    using System;
+    // The generic IComparer interface implements a method that compares 
+    // two objects. It is used in conjunction with the Sort and 
+    // BinarySearch methods on the Array, List, and SortedList classes.
+    public interface IComparer<in T>
+    {
+        // Compares two objects. An implementation of this method must return a
+        // value less than zero if x is less than y, zero if x is equal to y, or a
+        // value greater than zero if x is greater than y.
+        // 
+        int Compare(T x, T y);
+    }
+}
